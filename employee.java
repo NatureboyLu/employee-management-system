3ab982efc68eb42b717a3053dc2709910e8b0b2f
Lu Ding <108587907+NatureboyLu@users.noreply.github.com>
2022-12-12 17:56:51 -0700

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.employeemanagementsystem;
+
+/**
+ *
+ * @author luald
+ */
+public class employee {
+    private int id;
+    private String name;
+    private int age;
+    private String designation;
+    private String department;
+    private double salary;
+    
+  public int getId(){
+      return id;
+  }
+}
+
