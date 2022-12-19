6a78b5e88d9fc9baa7a19709b940b02c3f1b3392
Lu Ding <108587907+NatureboyLu@users.noreply.github.com>
2022-12-19 13:53:57 -0700

    Add files via upload

--- employee.java
+++ employee.java
@@ -1,8 +1,10 @@
+package com.mycompany.employeemanagementsystem;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.employeemanagementsystem;
+import java.io.Serializable;
 
 /**
  *
@@ -15,9 +17,69 @@ public class employee {
     private String designation;
     private String department;
     private double salary;
+
+    employee(int i, String james, int i0, String developer, String it) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
   public int getId(){
       return id;
   }
+  
+  public void setId(int id ){
+      this.id = id;
+      
+  }
+  public String getName(){
+     return name; 
+  }
+  
+  public void setName(String name){
+      this.name = name;
+  }
+  public int getAge(){
+      return age;
+  }
+  
+  public void setAge(int age){
+      this.age = age;
+  }
+  
+  public String getDesignation(){
+      return designation;
+  }
+  public void setDesignation(String designation){
+      this.designation = designation;
+  }
+  
+  public String getDepartment(){
+      return department;
+  }
+  public void setDepartment(String department){
+      this.department = department;
+  }
+  
+  public double getSalary(){
+      return salary;
+  }
+  public void setSalary(double salary){
+      this.salary = salary;
+  }
+    @Override
+  public String toString(){
+      return "Employee [id =" + id +",name =" +name + ",age=" + age + "designation=" +designation + ",department=" + department + ",salary=" + salary + "]";
+  }
+  
+  public  employee (int id, String name, int age, String designation, String department, double salary){
+      super ();
+      this.id = id;
+      this.name = name;
+      this.age = age;
+      this.designation = designation;
+      this.department = department;
+      this.salary = salary;
+  }
+  
+  
 }
 
